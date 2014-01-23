932e316f3e637e16d32cf21415359d7698399bac
Nick Reuter <nreuter85@gmail.com>
2014-01-23 13:50:42 -0600

    ruby: add modem driver
     * currently a copy of mdm.c from mach-msm
    
    Change-Id: I8cb3fe05089f14f760251df8e226edf3485b5b79

--- arch/arm/mach-msm/htc/ruby/board-ruby.c
+++ arch/arm/mach-msm/htc/ruby/board-ruby.c
@@ -1744,7 +1744,7 @@ struct platform_device msm_device_sdio_al = {
 #endif
 
 static struct platform_device *charm_devices[] __initdata = {
-	//&msm_charm_modem,
+	&msm_charm_modem,
 #ifdef CONFIG_MSM_SDIO_AL
 	&msm_device_sdio_al,
 #endif
