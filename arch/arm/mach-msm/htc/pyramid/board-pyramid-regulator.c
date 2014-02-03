e8e96bf3f69286d4ae9765b165bc5a49f85dab03
Flemmard <flemmard@gmail.com>
2014-02-03 11:28:52 +0100

    pyramid: fixup typo in regulator board

--- arch/arm/mach-msm/htc/pyramid/board-pyramid-regulator.c
+++ arch/arm/mach-msm/htc/pyramid/board-pyramid-regulator.c
@@ -415,6 +415,7 @@ void __init pyramid_pm8901_gpio_mpp_init(void)
 	 * MPP0 is low.
 	 */
         msm_gpio_regulator_pdata[GPIO_VREG_ID_EXT_5V].active_low = 0;
+        pm8901_vreg_mpp0.config.control = PM8XXX_MPP_DOUT_CTRL_LOW;
 
 	rc = pm8xxx_mpp_config(pm8901_vreg_mpp0.mpp, &pm8901_vreg_mpp0.config);
 	if (rc)
@@ -426,7 +427,7 @@ struct regulator_init_data msm_saw_regulator_pdata_s0 =
 	/*	      ID  vreg_name	       min_uV   max_uV */
 	SAW_VREG_INIT(PM8901_S0, "8901_s0",	       800000, 1325000);
 struct regulator_init_data msm_saw_regulator_pdata_s1 =
-	SAW_VREG_INIT(PM8901_S1, "8921_s1",	       800000, 1325000);
+	SAW_VREG_INIT(PM8901_S1, "8901_s1",	       800000, 1325000);
 
 /* RPM regulator constraints */
 static struct rpm_regulator_init_data rpm_regulator_init_data[] __devinitdata = {
